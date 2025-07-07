3347a12b2e06446314ac9402f9fde51fc8ecf955
bryamslm <bryamlopez4@gmail.com>
2025-07-07 11:01:57 -0600

    clabessi

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'nvnsptdwijqnetahaojg.supabase.co', // TU dominio de Supabase
+        hostname: 'otyixfcymgminntpwhtf.supabase.co', // TU dominio de Supabase
         pathname: '/storage/v1/object/public/**', // Permitir todas las imágenes en el bucket público
       },
     ],
